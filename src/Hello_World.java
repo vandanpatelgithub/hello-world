8e69caccffa4f1dd44898bf9adbc58ef8d6a1a84
vandanpatelgithub <vandan.ets@gmail.com>
2016-02-11 20:14:39 -0800

    Client asked me to change a comment
    The client asked for the comment to be added. I made that change
    for them.
    
    * Cilent asked for comment change
    * I don't see this affecting anything
    
    Closes-Bug: #129567

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,6 +1,10 @@
 
 public class Hello_World {
 
+	/**
+	 * Random Stuff
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World !");
